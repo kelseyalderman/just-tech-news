9a768bfa986f2a88fc6188965247b54ccd5a1b87
Kelsey Alderman <kelseyalderman@gmail.com>
2022-04-18 21:40:07 -0700

    create the post model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 // This file imports the User model and exports an object with it as a property
 
 const User = require("./User");
+const Post = require("./Post");
 
-module.exports = { User };
+module.exports = { User, Post };
